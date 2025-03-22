924f685792b58b337ab5e3cee663b3ce3413feec
Kalidash Odekare <shaktiadhikari49@gmail.com>
2025-03-22 23:31:37 +0600

    protect route update

--- src/hooks/userProtect.jsx
+++ src/hooks/userProtect.jsx
@@ -1,18 +1,34 @@
 import { useRouter } from 'next/router'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { FadeLoader } from 'react-spinners';
 
 const userProtect = ({ children }) => {
 
     const router = useRouter()
+    const [loading, setLoading] = useState(true);
 
     const token = typeof window !== 'undefined' ? localStorage.getItem("token") : null;
 
     useEffect(() => {
         if (!token) {
-            router.replace('/login')
-            return null;
+            router.replace('/login', undefined, { shallow: true }).then(() => {
+
+                setLoading(false)
+            })
+        } else {
+            setLoading(false)
         }
-    }, [token, router])
+    }, [router])
+
+    if (loading) {
+        return <div className='h-[600px] flex justify-center items-center'>
+            <FadeLoader
+                color="#07ab4e"
+                margin={5}
+            />
+        </div>
+    }
+
     return children
 }
 
